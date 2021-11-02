df4bae6c5ec0177f52e2b4e1afed4609bae7714b
ValeryNunez01 <72088086+ValeryNunez01@users.noreply.github.com>
2021-11-01 23:43:48 -0600

    RSA
    RSA

---
+++ RSA_Manita/src/rsa_manita/NewMain.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rsa_manita;
+
+import java.math.BigInteger;
+
+/**
+ *
+ * @author Valery
+ */
+public class NewMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        ventana vent = new ventana();
+        vent.setVisible(true);
+        vent.setLocationRelativeTo(null);
+    }
+    
+}
